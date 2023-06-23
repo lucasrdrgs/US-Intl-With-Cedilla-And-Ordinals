6220137970007b93c1e147e681c80c0bbe554387
Lucas Rodrigues <lucasrodriguespimentel@gmail.com>
2023-06-23 15:56:53 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,18 +5,18 @@ if [ -z ${xdotool} ]; then
   exit 1
 fi
 COMPOSE_FILE='/usr/share/X11/locale/en_US.UTF-8/Compose'
-GTK2_FILE='/usr/lib/x86_64-linux-gnu/gtk-2.0/2.10.0/immodules.cache'
+#GTK2_FILE='/usr/lib/x86_64-linux-gnu/gtk-2.0/2.10.0/immodules.cache'
 GTK3_FILE='/usr/lib/x86_64-linux-gnu/gtk-3.0/3.0.0/immodules.cache'
 ENV_FILE='/etc/environment'
 sudo cp ${COMPOSE_FILE} ${COMPOSE_FILE}.bak
-sudo cp ${GTK2_FILE} ${GTK2_FILE}.bak
+#sudo cp ${GTK2_FILE} ${GTK2_FILE}.bak
 sudo cp ${GTK3_FILE} ${GTK3_FILE}.bak
 sudo sed --in-place -e 's/ć/ç/g' ${COMPOSE_FILE}
 sudo sed --in-place -e 's/Ć/Ç/g' ${COMPOSE_FILE}
 GTK_FILE_SEARCH_FOR='^"cedilla".*:en'
 GTK_FILE_SED_EXP='s/^\(\"cedilla\".*:wa\)/\1:en/g'
-grep -q ${GTK_FILE_SEARCH_FOR} ${GTK2_FILE}
-[ $? -eq 1 ] && sudo sed --in-place -e ${GTK_FILE_SED_EXP} ${GTK2_FILE}
+#grep -q ${GTK_FILE_SEARCH_FOR} ${GTK2_FILE}
+#[ $? -eq 1 ] && sudo sed --in-place -e ${GTK_FILE_SED_EXP} ${GTK2_FILE}
 grep -q ${GTK_FILE_SEARCH_FOR} ${GTK3_FILE}
 [ $? -eq 1 ] && sudo sed --in-place -e ${GTK_FILE_SED_EXP} ${GTK3_FILE}
 ENV_FILE_GTK_LINE='GTK_IM_MODULE=cedilla'
@@ -28,4 +28,4 @@ grep -q ${ENV_FILE_QT_LINE} ${ENV_FILE}
 setxkbmap -layout us -variant altgr-intl -option "compose:ralt" -option "nbsp:level3" -option "lv3:ralt_switch"
 xmodmap -e "keycode 42 = h H h H NoSymbol NoSymbol U203A U2039"
 xmodmap -e "keycode 52 = g G g G NoSymbol NoSymbol masculine ordfeminine"
-
+echo "Please log out then log in."
